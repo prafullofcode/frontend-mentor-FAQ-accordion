e8de24cd9731ccf972e1838ff6a2c5caef39b0d2
prafullofcode <dahal.praphul268@gmail.com>
2025-05-14 14:03:53 +0545

    transition added

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
+toggleDisplay(1);
+
 for (let i = 1; i <= 4; i++) {
   let qsnContainer = document.querySelector(`.qsn-container-${i}`);
   qsnContainer.addEventListener("click", function () {
     let answer = document.querySelector(`.ans-${i}`);
-    if (answer.classList.contains("hidden")) deactiveAll();
-    toggleDisplay(i);
+    if (answer.classList.contains("show")) {
+      deactiveAll();
+    } else {
+      deactiveAll();
+      toggleDisplay(i);
+    }
   });
 }
 
 function toggleDisplay(i) {
   let plusIcon = document.querySelector(`.plus-icon-${i}`);
   let minusIcon = document.querySelector(`.minus-icon-${i}`);
+  let answer = document.querySelector(`.ans-${i}`);
+
   minusIcon.classList.toggle("hidden");
   plusIcon.classList.toggle("hidden");
-  let answer = document.querySelector(`.ans-${i}`);
-  answer.classList.toggle("hidden");
+  answer.classList.toggle("show");
 }
 
 function deactiveAll() {
   for (let i = 1; i <= 4; i++) {
     let answer = document.querySelector(`.ans-${i}`);
-    answer.classList.add("hidden");
+    answer.classList.remove("show");
+
     let plusIcon = document.querySelector(`.plus-icon-${i}`);
     let minusIcon = document.querySelector(`.minus-icon-${i}`);
     minusIcon.classList.add("hidden");
